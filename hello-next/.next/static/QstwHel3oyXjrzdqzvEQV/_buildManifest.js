283e2ecef6593a3e483a7a084c2fdf3e1c959f18
Tiger <tiger.dingsun@gmail.com>
2020-02-13 17:01:46 -0500

    initial push

---
+++ hello-next/.next/static/QstwHel3oyXjrzdqzvEQV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/p/[id]":[a]}}("static\u002Fchunks\u002Ffcba5afdd346961be4d1073b3a0b38418d5db66b.6c44f0048d1351987336.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ hello-next/pages/about.js
@@ -0,0 +1,13 @@
+import Header from '../comps/Header';
+import Layout from '../comps/MyLayout';
+
+export default function About() {
+    return (
+        <div>
+            <Header />
+            <Layout>
+                <p>This is the about page</p>
+            </Layout>
+        </div>
+    )
+}
